8afdacbc5579d43f08a0d7bc65bd4ad78ec5c8b7
刘白菜 <liushuai.baicai@hotmail.com>
2018-05-29 22:44:45 +0800

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details onbu the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
