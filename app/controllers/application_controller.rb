03451af89658ad76cfbfaa78b625911ce41429ea
刘白菜 <liushuai.baicai@hotmail.com>
2018-06-02 13:59:34 +0800

    update mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :mailer_set_url_options
+
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options = {:host => request.host_with_port}
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default :charset => "utf-8"
